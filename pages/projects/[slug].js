8b050bdcdd47757919d0c3f226cd08be1a1b8c31
Daniel Alvarez <daniel@alvarez.tech>
2022-02-07 20:47:45 -0800

    Projects module added

---
+++ pages/projects/[slug].js
@@ -0,0 +1,49 @@
+import Layout from '../../components/layout';
+import { getPostSlugs, getPost } from '../../lib/posts';
+import { classNames } from '../../lib/util';
+import { getMDXComponent } from "mdx-bundler/client";
+import MDXComponent from '../../components/mdxComponents';
+import GitHub from '../../components/github';
+import { useMemo } from "react";
+
+const components = { GitHub, ...MDXComponent }
+
+export default function Project({ project }) {
+  const Component = useMemo(() => getMDXComponent(project.code), project.code);
+  return (
+    <Layout>
+
+      <div
+        className="bg-white max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
+        <div
+          className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
+          <div>
+            <h2
+              className={classNames("uppercase", 'inline-flex items-center px-2 rounded text-xl font-bold')}>{project.title}</h2>
+          </div>
+
+        </div>
+        <div className="content">
+          <Component components={components} />
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
+
+export async function getStaticPaths() {
+  const paths = getPostSlugs('projects')
+  return {
+    paths, fallback: false
+  }
+}
+
+export async function getStaticProps({ params }) {
+  const project = await getPost(params.slug, 'projects')
+  return {
+    props: {
+      project
+    }
+  }
+}
