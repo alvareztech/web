ee22c65b1984bc52241876cbc87493b089df5452
Daniel Alvarez <daniel@alvarez.tech>
2022-02-09 11:49:16 -0800

    Fix youtube component

--- components/youtube.js
+++ components/youtube.js
@@ -1,12 +1,12 @@
 export default function YouTube({ videoId }) {
   return (
-    <div class="relative" style={{ 'padding-top': '56.25%' }}>
+    <div className="relative" style={{ paddingTop: '56.25%' }}>
       <iframe
         className="absolute inset-0 w-full h-full"
         src={"https://www.youtube.com/embed/" + videoId}
         title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen>
       </iframe>
     </div>
   )
