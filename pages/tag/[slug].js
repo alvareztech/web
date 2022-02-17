d971430b9b16931ceddef689d14ac7c6fa585f98
Daniel Alvarez <daniel@alvarez.tech>
2022-02-17 14:43:19 -0800

    Tag color page improvement

--- pages/tag/[slug].js
+++ pages/tag/[slug].js
@@ -17,7 +17,7 @@ export default function Tag({ tag, posts }) {
           className="relative max-w-lg mx-auto divide-y-2 divide-gray-200 lg:max-w-7xl">
           <div>
             <h2
-              className={classNames('text-white', "uppercase", "bg-gray-500", 'inline-flex items-center px-2 rounded text-xl font-bold')}>{tag}</h2>
+              className={classNames('text-white', "uppercase", getColor(tag), 'inline-flex items-center px-2 rounded text-xl font-bold')}>{tag}</h2>
           </div>
 
           <div
