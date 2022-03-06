896251e1e7fc4f8f78d9b115ee81a4a6e1195c0f
Daniel Alvarez <daniel@alvarez.tech>
2022-03-06 11:53:10 -0800

    Contact title added

--- pages/contact.js
+++ pages/contact.js
@@ -1,9 +1,13 @@
 import Layout from '../components/layout'
 import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
+import Head from 'next/head'
 
 export default function Contact() {
   return (
     <Layout>
+      <Head>
+        <title>Contact - ALVAREZ.tech</title>
+      </Head>
       <div className="bg-white">
         <div className="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
           <div className="max-w-lg mx-auto md:max-w-none md:grid md:grid-cols-2 md:gap-8">
