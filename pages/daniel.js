fc9670d847db66a7fff1bc631b95b61b56b97a89
Daniel Alvarez <daniel@alvarez.tech>
2022-08-17 10:25:17 -0700

    Rest

---
+++ pages/daniel.js
@@ -0,0 +1,106 @@
+import Layout from '../components/layout'
+import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
+import { CameraIcon } from '@heroicons/react/solid'
+import Head from 'next/head'
+
+export default function Daniel() {
+  return (
+    <Layout>
+      <Head>
+        <title>Daniel Alvarez - ALVAREZ.tech</title>
+        <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+      </Head>
+      <div className="bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+          <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
+          <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+            <div>
+              <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Software Engineer</h2>
+              <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+                Daniel A. Alvarez A.
+              </h3>
+            </div>
+          </div>
+          <div className="mt-8 lg:grid lg:grid-cols-2 lg:gap-8">
+            <div className="relative lg:row-start-1 lg:col-start-2">
+              <svg
+                className="hidden lg:block absolute top-0 right-0 -mt-20 -mr-20"
+                width={404}
+                height={384}
+                fill="none"
+                viewBox="0 0 404 384"
+                aria-hidden="true"
+              >
+                <defs>
+                  <pattern
+                    id="de316486-4a29-4312-bdfc-fbce2132a2c1"
+                    x={0}
+                    y={0}
+                    width={20}
+                    height={20}
+                    patternUnits="userSpaceOnUse"
+                  >
+                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                  </pattern>
+                </defs>
+                <rect width={404} height={384} fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
+              </svg>
+              <div className="relative text-base mx-auto max-w-prose lg:max-w-none">
+                <figure>
+                  <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
+                    <img
+                      className="rounded-lg shadow-lg object-cover object-center"
+                      src="images/danielsf.JPG"
+                      alt="Whitney leaning against a railing on a downtown street"
+                      width={1184}
+                      height={1376}
+                    />
+                  </div>
+                  <figcaption className="mt-3 flex text-sm text-gray-500">
+                    <CameraIcon className="flex-none w-5 h-5 text-gray-400" aria-hidden="true" />
+                    <span className="ml-2">San Francisco</span>
+                  </figcaption>
+                </figure>
+              </div>
+            </div>
+            <div className="mt-8 lg:mt-0">
+              <div className="text-base max-w-prose mx-auto lg:max-w-none">
+                <p className="text-lg text-gray-500">
+                  iOS/Android Software Engineer. Around 10 years working professionally for technology companies of different areas, financial, health care, IoT, retail, government. Developing native iOS and Android apps (Swift/Obj-C, Kotlin/Java) and creating native plugins for hybrid frameworks. Developing mobile SDKs for iOS and Android.
+                </p>
+              </div>
+              <div className="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
+                <p>
+                  Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
+                  pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
+                </p>
+                <p>
+                  Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.
+                </p>
+                <ul role="list">
+                  <li>Quis elit egestas venenatis mattis dignissim.</li>
+                  <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
+                  <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
+                </ul>
+                <p>
+                  Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
+                  velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
+                  egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
+                </p>
+                <h3>How we helped</h3>
+                <p>
+                  <div class="badge-base LI-profile-badge" data-locale="es_ES" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="alvareztech" data-version="v1"><a class="badge-base__link LI-simple-link" href="https://mx.linkedin.com/in/alvareztech?trk=profile-badge">Daniel Alvarez</a></div>
+                </p>
+                <p>
+                  Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
+                  pellentesque. Blandit amet, sed aenean erat arcu morbi.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </Layout>
+  )
+}
---
+++ public/github/pr-template-2.md
@@ -0,0 +1,21 @@
+# Jira
+<!-- Link to Jira ticket -->
+
+# Purpose
+[Describe the problem or feature in addition to a link to the issues.]
+
+# Documentation:
+<!-- Updates to documentation replace items in this section as needed -->
+No requirement updates
+No Software Architecture Document updates
+No Software Design Specification updates
+
+# SOUP:
+<!-- Updates to SOUP replace items in this section as needed -->
+No SOUP was used
+
+# SRS
+link in Jira tickets
+
+# Visual designs:
+link in Jira tickets
