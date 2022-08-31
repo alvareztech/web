d368241503f0cce18a59af42053400337bec8b95
Daniel Alvarez <daniel@alvarez.tech>
2022-08-30 22:46:51 -0700

    updates

--- pages/[slug].js
+++ pages/[slug].js
@@ -138,7 +138,7 @@ export default function Post({post}) {
                   <a>
                     <span
                       key={tag}
-                      className="uppercase inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200">
+                      className="uppercase inline-flex items-center px-2.5 py-0.5 rounded text-xs font-medium bg-gray-100 text-gray-800">
                       {tag}
                     </span>
                   </a>
