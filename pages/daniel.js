10ba1d4763bd40d9738bf936bdf2985147e00208
Daniel Alvarez <daniel@alvarez.tech>
2022-08-30 22:02:10 -0700

    new project

--- pages/daniel.js
+++ pages/daniel.js
@@ -1,22 +1,29 @@
 import Layout from '../components/layout'
-import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
-import { CameraIcon } from '@heroicons/react/solid'
+import {MailIcon, PhoneIcon} from '@heroicons/react/outline'
+import {CameraIcon} from '@heroicons/react/solid'
 import Head from 'next/head'
+import Image from "next/image";
 
 export default function Daniel() {
   return (
     <Layout>
       <Head>
         <title>Daniel Alvarez - ALVAREZ.tech</title>
-        <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
+        <script src="https://platform.linkedin.com/badges/js/profile.js" async
+                defer type="text/javascript"></script>
       </Head>
       <div className="bg-white overflow-hidden">
         <div className="relative max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
-          <div className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen" />
-          <div className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
+          <div
+            className="hidden lg:block bg-gray-50 absolute top-0 bottom-0 left-3/4 w-screen"/>
+          <div
+            className="mx-auto text-base max-w-prose lg:grid lg:grid-cols-2 lg:gap-8 lg:max-w-none">
             <div>
-              <h2 className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Software Engineer</h2>
-              <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              <h2
+                className="text-base text-indigo-600 font-semibold tracking-wide uppercase">Software
+                Engineer</h2>
+              <h3
+                className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
                 Daniel A. Alvarez A.
               </h3>
             </div>
@@ -40,24 +47,28 @@ export default function Daniel() {
                     height={20}
                     patternUnits="userSpaceOnUse"
                   >
-                    <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                    <rect x={0} y={0} width={4} height={4}
+                          className="text-gray-200" fill="currentColor"/>
                   </pattern>
                 </defs>
-                <rect width={404} height={384} fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)" />
+                <rect width={404} height={384}
+                      fill="url(#de316486-4a29-4312-bdfc-fbce2132a2c1)"/>
               </svg>
-              <div className="relative text-base mx-auto max-w-prose lg:max-w-none">
+              <div
+                className="relative text-base mx-auto max-w-prose lg:max-w-none">
                 <figure>
                   <div className="aspect-w-12 aspect-h-7 lg:aspect-none">
-                    <img
+                    <Image
                       className="rounded-lg shadow-lg object-cover object-center"
-                      src="images/danielsf.JPG"
-                      alt="Whitney leaning against a railing on a downtown street"
+                      src="/images/danielsf.JPG"
+                      alt="Daniel Alvarez"
                       width={1184}
                       height={1376}
                     />
                   </div>
                   <figcaption className="mt-3 flex text-sm text-gray-500">
-                    <CameraIcon className="flex-none w-5 h-5 text-gray-400" aria-hidden="true" />
+                    <CameraIcon className="flex-none w-5 h-5 text-gray-400"
+                                aria-hidden="true"/>
                     <span className="ml-2">San Francisco</span>
                   </figcaption>
                 </figure>
@@ -66,36 +77,32 @@ export default function Daniel() {
             <div className="mt-8 lg:mt-0">
               <div className="text-base max-w-prose mx-auto lg:max-w-none">
                 <p className="text-lg text-gray-500">
-                  iOS/Android Software Engineer. Around 10 years working professionally for technology companies of different areas, financial, health care, IoT, retail, government. Developing native iOS and Android apps (Swift/Obj-C, Kotlin/Java) and creating native plugins for hybrid frameworks. Developing mobile SDKs for iOS and Android.
+                  I have developed world-class iOS and Android applications for
+                  different areas such as healthcare, finance, IoT, retail and
+                  government. Developed SDKs for Android and iOS, created native
+                  plugins for hybrid mobile frameworks, and automated
+                  development processes.
+                </p>
+                <p>
+                  I use my deep knowledge in both platforms to work efficiently,
+                  sync features, optimize resources usage and lead teams.
                 </p>
               </div>
-              <div className="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
+              <div
+                className="mt-5 prose prose-indigo text-gray-500 mx-auto lg:max-w-none lg:row-start-1 lg:col-start-1">
                 <p>
-                  Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
-                  pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
+                  I write unit tests, integration tests, and UI tests (XCTest,
+                  JUnit, Espresso, Mockito) at different stages of development.
+                  Trying to adopt TDD for new and specific projects.
                 </p>
                 <p>
-                  Dictum magnis risus phasellus vitae quam morbi. Quis lorem lorem arcu, metus, egestas netus cursus. In.
-                </p>
-                <ul role="list">
-                  <li>Quis elit egestas venenatis mattis dignissim.</li>
-                  <li>Cras cras lobortis vitae vivamus ultricies facilisis tempus.</li>
-                  <li>Orci in sit morbi dignissim metus diam arcu pretium.</li>
-                </ul>
-                <p>
-                  Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
-                  velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
-                  egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
-                </p>
-                <h3>How we helped</h3>
-                <p>
-                  <div className="badge-base LI-profile-badge" data-locale="es_ES" data-size="medium" data-theme="dark" data-type="VERTICAL" data-vanity="alvareztech" data-version="v1">
-                    <a className="badge-base__link LI-simple-link" href="https://mx.linkedin.com/in/alvareztech?trk=profile-badge">Daniel Alvarez</a>
-                  </div>
-                </p>
-                <p>
-                  Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque tristique
-                  pellentesque. Blandit amet, sed aenean erat arcu morbi.
+                  I like to test and adopt new technologies for personal
+                  projects and suggest them when they reach a mature level for
+                  professional projects. I have several talks and conferences as
+                  a speaker. Collaborator in technology communities and attendee
+                  to the most important developer events, Google I/O and Apple
+                  WWDC. I like to share, I create content, video tutorials,
+                  developer resources for my website.
                 </p>
               </div>
             </div>
