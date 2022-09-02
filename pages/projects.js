a1495ad07280a6663ef821222eec8b11dac4cbd2
Daniel Alvarez <daniel@alvarez.tech>
2022-09-02 10:59:53 -0700

    improvements

--- pages/projects.js
+++ pages/projects.js
@@ -19,8 +19,17 @@ export default function Projects({ projects }) {
           <div className="text-center">
             <h2 className="text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl">{titleProjects}</h2>
             <p className="mt-3 max-w-2xl mx-auto text-xl text-gray-500 sm:mt-4">
-              {subtitleProjects}
+              Some small personal projects I was working on and am working on now.
             </p>
+            <p className="text-xl text-gray-500">
+              All open source 😉
+            </p>
+            <p className="mt-4 text-xl text-gray-500">
+              This site is also a project.
+            </p>
+            <Link href={"/about"}>
+              <a className="text-blue-500 text-base font-medium hover:text-blue-300">About this site</a>
+            </Link>
           </div>
           <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
             {projects.map((project) => (
