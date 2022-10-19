54bfcc8911fe1a3e8ab24c1fe7906ac1cd6f2dfa
Daniel Alvarez <daniel@alvarez.tech>
2022-10-19 10:25:45 -0700

    Fix components

--- components/date.js
+++ components/date.js
@@ -1,6 +1,10 @@
 import { parseISO, format } from 'date-fns'
 
 export default function Date({ dateString }) {
-  const date = parseISO(dateString)
-  return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  if (dateString) {
+    const date = parseISO(dateString)
+    return <time dateTime={dateString}>{format(date, 'LLLL d, yyyy')}</time>
+  } else {
+    return <></>
+  }
 }
