444b0fd336331d43b00786633bbfeef20028c313
Daniel Alvarez <daniel@alvarez.tech>
2022-11-10 23:12:52 -0800

    Firebase Firestore implementation

---
+++ app/quotes/page.js
@@ -0,0 +1,24 @@
+import { db } from '../../firebase/config'
+import { collection, doc, getDocs, query, addDoc } from '@firebase/firestore'
+
+async function getQuotes() {
+  const quotes = query(collection(db, 'quotes'))
+  const querySnapshot = await getDocs(quotes)
+  const data = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
+  return data
+}
+
+export default async function Page() {
+  const quotes = await getQuotes();
+  return (<>
+    <h2 className='m-4 text-colorAccent text-3xl'>Quotes</h2>
+    {
+      quotes.map((quote) => (
+        <div className='m-4'>
+          <p className='dark:text-white text-2xl'>{quote.content}</p>
+          <p className='dark:text-white'>{quote.author}</p>
+        </div>
+      ))
+    }
+  </>)
+}
