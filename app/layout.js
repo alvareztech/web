37103dd30812ab2bbeba1aad5ae50c69aabab69d
Daniel Alvarez <daniel@alvarez.tech>
2022-11-11 10:23:04 -0800

    Layout title added

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ export default function RootLayout({ children }) {
   return (
     <html>
       <head>
+        <title>Daniel Alvarez</title>
       </head>
       <body className="bg-white dark:bg-black">{children}</body>
     </html>
