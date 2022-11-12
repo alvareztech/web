974917fba52b8c1fbd263803e900ddde847e1e53
Daniel Alvarez <daniel@alvarez.tech>
2022-11-11 18:21:00 -0800

    Remove Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,10 @@
 import { Analytics } from '@vercel/analytics/react';
-import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <>
-      <Script strategy='afterInteractive'
-        src='https://www.googletagmanager.com/gtag/js?id=G-95Q6MRZKP7' />
-      <Script id='google-analytics' strategy='afterInteractive'>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-95Q6MRZKP7');`
-        }
-      </Script>
       <Component {...pageProps} />
       <Analytics />
     </>
